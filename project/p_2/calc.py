f1fffd73b991fcb9f56971bc0e893ef01f63f2f7
Gustavo Rocha <gurocha05@gmail.com>
2025-02-14 16:08:40 -0300

    Update calc.py

--- project/p_2/calc.py
+++ project/p_2/calc.py
@@ -9,5 +9,5 @@ print(2*3)
 print(-3*4.5)
 #division
 print(9/3)
-print(10//3)#only full division
-print(10%7)#only the other part
+print(10//7)#only the integer division
+print(10%7)#only the non-integer part
