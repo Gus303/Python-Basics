fd046c622474afce6ec2632920e0e0ab7e272b66
Gustavo Rocha <gurocha05@gmail.com>
2025-02-15 17:00:56 -0300

    Update var.py

--- project/p_3/var.py
+++ project/p_3/var.py
@@ -1,5 +1,16 @@
-#number
-seven = 7
-print(seven)
-print(seven+2)
+# A variable is like a container that stores a value
+
+# Creating a variable and assigning a value
+name = "Alice"  # This variable stores a string
+age = 25        # This variable stores a number
+
+# Printing the values of the variables
+print("Name:", name)
+print("Age:", age)
+
+# Changing the value of a variable
+age = age + 1  # Now the age is increased by 1
+
+# Printing the updated value
+print("New Age:", age)
 
