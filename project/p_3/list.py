d7a95e41ee3fe4f563daa203f0690650eff52cf0
Gustavo Rocha <gurocha05@gmail.com>
2025-02-15 17:00:20 -0300

    Create list.py

---
+++ project/p_3/list.py
@@ -0,0 +1,22 @@
+# A list is a collection of items stored in a single variable
+
+# Creating a list of fruits
+fruits = ["Apple", "Banana", "Cherry"]
+
+# Printing the whole list
+print("Fruit list:", fruits)
+
+# Accessing individual items
+print("First fruit:", fruits[0])  # Index 0 refers to the first item
+
+# Adding a new fruit to the list
+fruits.append("Orange")
+
+# Printing the updated list
+print("Updated fruit list:", fruits)
+
+# Removing an item from the list
+fruits.remove("Banana")
+
+# Printing the final list
+print("Final fruit list:", fruits)
