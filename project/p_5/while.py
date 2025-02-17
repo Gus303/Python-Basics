9eb77826582a332c98c51e8418f693fbe17b5e4b
Gustavo Rocha <gurocha05@gmail.com>
2025-02-17 16:32:21 -0300

    Create while.py

---
+++ project/p_5/while.py
@@ -0,0 +1,12 @@
+#number = 0
+#while number <= 10:
+##    print(number)
+#    number += 1 #number = number + 1
+#print('The loop finished')
+
+number_max = eval(input('Enter a number:'))
+number = 0
+while number <= number_max:
+    print(number)
+    number += 1
+print('The loop finished')
