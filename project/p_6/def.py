71fd43ecdba1c237a8af3889437ce2d314eb3487
Gustavo Rocha <gurocha05@gmail.com>
2025-02-17 20:53:49 -0300

    Create def.py

---
+++ project/p_6/def.py
@@ -0,0 +1,10 @@
+def hello_world():
+    #Function that shows a text
+    print('Hello World!')
+hello_world()
+
+def function_name(name='User'):
+    print(f'Hello, my name is {name}')
+name = input("What is your name: ")
+function_name()
+
