6780dae355f3483e6f1b74d16925b4d906298320
Gustavo Rocha <gurocha05@gmail.com>
2025-02-17 15:59:49 -0300

    Create evens_or_odds.py

---
+++ project/p_4/evens_or_odds.py
@@ -0,0 +1,15 @@
+#number_typed=eval(input('Enter a number:'))
+
+#randomnumber = number_typed % 2
+#randomnumber == 0
+
+#print(randomnumber)
+
+#####
+
+number = eval(input('Enter a number:'))
+
+if number % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
