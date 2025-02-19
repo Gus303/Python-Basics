0e4774adfeac92bcfeb104ba0e8c6502c791156d
Gustavo Rocha <gurocha05@gmail.com>
2025-02-19 14:31:06 -0300

    Update index-string.py

--- project/p_7/index-string.py
+++ project/p_7/index-string.py
@@ -6,3 +6,9 @@ hello_slice=hello[0:5]
 print(hello_slice)
 world_slice=hello[6:11]
 print(world_slice)
+
+print(hello.count('o'))
+print(hello.lower())
+print(hello.upper())
+
+print(hello.split('Hello'))
