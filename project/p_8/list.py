8351976760e3a5dee7a5cf39d7a7fc7f18af1fff
Gustavo Rocha <gurocha05@gmail.com>
2025-02-20 14:56:24 -0300

    Update list.py

--- project/p_8/list.py
+++ project/p_8/list.py
@@ -10,7 +10,22 @@ more_animals = ['duck', 'hamster', 'fish']
 new_list = animals + more_animals
 print(new_list)
 
+new_list.append('frog')
+print(new_list)
+
+print(new_list.sort)
+
+new_list.insert(3, 'lion')
+new_list.sort()
+print(new_list)
+
+new_list.reverse()
+print(new_list)
+
 numbers = [1, 2, 3, 4]
 print(numbers)
 print(max(numbers))
 print(min(numbers))
+
+numbers.append(5)
+print(numbers)
