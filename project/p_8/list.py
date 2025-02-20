0a7e01332b2e7b052d8dce07c540fb368bb7fe12
Gustavo Rocha <gurocha05@gmail.com>
2025-02-20 14:51:09 -0300

    Update list.py

--- project/p_8/list.py
+++ project/p_8/list.py
@@ -1,3 +1,16 @@
 animals = ['dog', 'cat', 'bird']
 print(animals)
 print(type(animals))
+
+print(animals[0])
+print(len(animals))
+
+more_animals = ['duck', 'hamster', 'fish']
+
+new_list = animals + more_animals
+print(new_list)
+
+numbers = [1, 2, 3, 4]
+print(numbers)
+print(max(numbers))
+print(min(numbers))
